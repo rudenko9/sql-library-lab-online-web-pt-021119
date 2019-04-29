daaea965a05fac430cffbe8ed369726e24a17341
Nataliya R <n.rudenko@gmail.com>
2019-04-29 18:58:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,5 +35,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  ""
+  "SELECT characters.name, COUNT(name)
+  FROM character_book 
+  INNER JOIN characters 
+  ON characters.id = character_book.character_id 
+  GROUP BY character.name 
+  ORDER BY COUNT(name) DESC;"
 end
