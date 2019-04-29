c67cf142edd4a65f200fd5e1ac56242fb9f8c9e1
Nataliya R <n.rudenko@gmail.com>
2019-04-29 18:18:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, SUM(species) 
-  FROM characters ORDER BY species 
+  FROM characters GROUP BY species 
+  ORDER BY COUNT(species) DESC
   LIMIT 1;"
 end
 
