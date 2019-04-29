ca0aed2ff38fa4b52817c9e3746a544e3e07e2a6
Nataliya R <n.rudenko@gmail.com>
2019-04-29 17:38:49 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE series ( id INTEGER PRIMARY KEY,
 title TEXT, 
 author_id INTEGER,
-subgenre_id INTEGER)
+subgenre_id INTEGER);
 
 CREATE TABLE subgenres(id INTEGER PRIMARY KEY,
-name TEXT)
+name TEXT);
 
 CREATE TABLE authors (id INTEGER PRIMARY KEY,
-name TEXT)
+name TEXT);
 
 CREATE TABLE books (id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER,
-series_id INTEGER)
+series_id INTEGER);
